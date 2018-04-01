35d169f7d5e0c29a445f35013ca79fc7eec6558c
Antony <Toxa23A@gmail.com>
2018-04-01 22:06:52 +0300

    add user delete rout

--- api/routing/user.routing.js
+++ api/routing/user.routing.js
@@ -6,27 +6,36 @@ const User = require('../models/user.model');
 
 router.post('/singup', (req, res) => { 
     User.find({email: req.body.email})
-    .then(users => {
-        if (users.length >= 1) {
-            res.status(409).json({
-                error: 'Email already exists!'
-            })
-        }else{
-            new User({
-                _id: mongoose.Types.ObjectId(),
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                createdAt: new Date()
-            }).save((err, doc)=>{
-                if (err) {
-                    res.status(500).json({error: err})
-                }
-                res.status(200).json(doc);
-            })
-        }
-    })
-    
+        .then(users => {
+            if (users.length >= 1) {
+                res.status(409).json({
+                    error: 'Email already exists!'
+                })
+            }else{
+                new User({
+                    _id: mongoose.Types.ObjectId(),
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    createdAt: new Date()
+                }).save((err, doc)=>{
+                    if (err) {
+                        res.status(500).json({error: err})
+                    }
+                    res.status(200).json(doc);
+                })
+            }
+        })
  })
+
+ router.delete('/:userId', (req, res) => { 
+     User.remove({_id: req.params.userId})
+        .then(delRes=>{
+            res.status(200).json({message: 'User deleted'})
+        })
+        .catch(err=>{
+            res.status(500).json({error: err})
+        })
+  })
 
  module.exports = router
